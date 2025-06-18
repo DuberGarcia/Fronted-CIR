a37ae16b3a433836afbe989845942a4fd4491875
DuberGarcia <duber.garcia@solucionessyh.com>
2025-06-18 16:08:03 -0500

    feat: se agrego componentes ui y layout

---
+++ src/app/layout/moduleLayout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-layout',
+  imports: [],
+  templateUrl: './moduleLayout.component.html',
+  styleUrl: './moduleLayout.component.css',
+})
+export class ModuleLayoutComponent { }
